102f1cf98e6c60691ccde00071c0c305bd6984c1
Denys <chornijj14@gmail.com>
2020-04-29 20:36:46 +0300

    Change manifest file

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ replaceText(document.body);
 function replaceText(element){
     if(element.hasChildNodes()){
         element.childNodes.forEach(replaceText)
+    }else if(element.nodeType === Text.TEXT_NODE){
+        element.textContent = element.textContent.replace(/coronavirus/gi, 'Love each other');
+        element.textContent = element.textContent.replace(/covid-19/gi, 'Love each other');
+
     }
 }
